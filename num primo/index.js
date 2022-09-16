6fecd1b3142f968729f0d781e715bf5915325a20
Érica Joice dos Santos <ejoice19@gmail.com>
2022-09-16 10:11:58 -0300

    Exercicio numero primos

---
+++ num primo/index.js
@@ -0,0 +1,11 @@
+function primeNumber(num) {
+   for (var divisor = 2; divisor < num; divisor++) 
+   if (num % divisor == 0) return false;
+   return true;
+}
+
+var determinadoNumero = 992;
+
+for (var i = 2; i < determinadoNumero; i++) if (primeNumber(i)) console.log(i)
+
+
