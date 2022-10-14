ec2b5504436e2450b0aaca76523362c8c2a5aaa9
Érica Joice dos Santos <ejoice19@gmail.com>
2022-10-14 15:41:54 -0300

    limpando os contadores

--- relogio/assets/js/main.js
+++ relogio/assets/js/main.js
@@ -3,13 +3,14 @@ const pausarBtn = document.querySelector(".pausar");
 const zerarBtn = document.querySelector(".zerar");
 const relogiobtn = document.querySelector(".relogio");
 let segundos = 0;
+let timer;
 
 function inciaRelogio(){
-   const timer = setInterval(function() {
+   timer = setInterval(function() {
       segundos++;
       relogiobtn.innerHTML = criaHoradosSegundos(segundos); //segundos formatados
    }, 1000)
-}
+};
 
 //configurar a hora
 function criaHoradosSegundos(segundos){
@@ -25,14 +26,19 @@ console.log(criaHoradosSegundos(10))
 
 //add click
 iniciarBtn.addEventListener('click', function(event){
+   clearInterval(timer); 
    inciaRelogio();
+   relogiobtn.style.color = ' black';
 });
 
 pausarBtn.addEventListener('click', function(event){
-      
+     clearInterval(timer); 
+     relogiobtn.style.color = 'red'
 });
 
 zerarBtn.addEventListener('click', function(event){
-      
+     clearInterval(timer);
+     relogiobtn.innerHTML = '00:00:00' 
+     segundos = '0'
 });
 
